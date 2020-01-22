7521f12696b2095e0040bcec6f6990e48b58db60
Xavier Morales <xavier.morales@amaris.com>
2020-01-22 12:33:30 +0100

    Add practica 4 content

---
+++ practica4/outputs.tf
@@ -0,0 +1,10 @@
+output "vm_fqdn" {
+  value = azurerm_public_ip.example.fqdn
+}
+output "private_key" {
+  value = tls_private_key.example.private_key_pem
+}
+output "ssh_command" {
+  value = "ssh ${local.admin_username}@${azurerm_public_ip.example.ip_address}"
+}
+
